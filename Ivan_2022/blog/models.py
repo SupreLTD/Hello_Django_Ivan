1fa9662cc028c2fcda53b5a9388470dd72757e7c
Vanya <balanaaarrrr@gmail.com>
2022-10-23 14:32:40 +0300

    Blog app

--- Ivan_2022/blog/models.py
+++ Ivan_2022/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_updates = models.DateTimeField(auto_now=True)
+    category = models.ManyToManyField('Category', related_name='posts', blank=True)
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=60)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    posts = models.ForeignKey('Post', on_delete=models.CASCADE)
